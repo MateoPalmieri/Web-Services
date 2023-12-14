9c4fd639e5a3ee3776cc772e7d9e9f51394b3710
Matéo PALMIERI <120114578+MateoPalmieri@users.noreply.github.com>
2023-12-14 19:25:48 +0100

    Remove unused endpoints

--- 1. Creation WebService Movie/index.js
+++ 1. Creation WebService Movie/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 const port = 3000;
+const fs = require('fs');
 const films = require('./films.json');
 
 // Middleware to parse request body in JSON
 app.use(express.json());
 
 // Le WebService en lui même
-//     Il doit disposer de routes de récupération des films (en listing, et récupération d’entité)
+//     ✅ Il doit disposer de routes de récupération des films (en listing, et récupération d’entité)
 //     Il doit être possible de modifier un film
 //     Il doit être possible de créer un film
 //     Il doit être possible de supprimer un film
@@ -21,9 +22,9 @@ app.use(express.json());
 
 // Les codes de retour minimum
 // A minima les codes de retour suivant doivent être implémentés :
-//     200 pour une récupération de ressource ou listing ou modification avec succès
+//     ✅ 200 pour une récupération de ressource ou listing ou modification avec succès
 //     201 création avec succès
-//     404 ressource absente
+//     ✅ 404 ressource absente
 //     422 validation impossible
 
 // Et tout autre code a votre convenance du moment que sa description (https://fr.wikipedia.org/wiki/Liste_des_codes_HTTP) reste cohérente
@@ -42,22 +43,12 @@ app.use(express.json());
 
 // MAJ pour le rendu du 15 décembre 2023
 //
-// Il doit permettre de rechercher par titre ou par description (petit indice, sur une route de collection on peu passer des paramètres , les query)
-// Le résultat doit être paginé (page 1 sur 22, 10 éléments par page par exemple)
-// Un film doit être attaché à une ou plusieurs catégories
-// On doit pouvoir lister les catégories d’un film et inversement (la notions des sous-ressources et leurs URI)
+// ⬛ Il doit permettre de rechercher par titre ou par description (petit indice, sur une route de collection on peu passer des paramètres , les query)
+// ✅ Le résultat doit être paginé (page 1 sur 22, 10 éléments par page par exemple)
+// ✅ Un film doit être attaché à une ou plusieurs catégories
+// ⬛ On doit pouvoir lister les catégories d’un film et inversement (la notions des sous-ressources et leurs URI)
 // Il faut penser au développeur qui va utiliser le webservice … Personne ne lit la doc (RTFM en est né), il devra pouvoir retourner du JSON HAL
 
-
-// GET method route
-app.get('/', function (req, res) {
-    res.send('GET request to the homepage');
-});
-
-// POST method route
-app.post('/', function (req, res) {
-    res.send('POST request to the homepage');
-});
 
 // Endpoint to get every films
 app.get('/films', (req, res) => {
@@ -75,6 +66,27 @@ app.get('/films/:id', (req, res) => {
     } else {
         res.status(404).json({ error: 'Film not found' });
     }
+});
+
+
+// Endpoint to delete a film by his ID
+app.delete('/films/delete/:id', (req, res) => {
+
+  // Recherche du film dans la liste
+  const filmId = parseInt(req.params.id);
+  const film = films.findIndex((film) => film.id === filmId);
+
+  if (film !== -1) {
+    // Delete the movie
+    films.splice(film, 1);
+
+    // Save the modification
+    fs.writeFile('./films.json', JSON.stringify(films, null, 2));
+
+    res.json({ success: true, message: 'Film deleted.' });
+  } else {
+    res.status(404).json({ success: false, message: 'Film not found.' });
+  }
 });
 
 
@@ -132,8 +144,7 @@ app.get('/page/:page', (req, res) => {
   
     // Send paginated films in response
     res.json(paginatedFilms);
-  });
-
+});
 
 
 // Start the server
